acf44d746324d1b5a869cc7c4f84130b5fe9b68c
Jon Craton <jncraton@gmail.com>
2016-02-12 17:49:31 -0500

    Run tests simultaneously

--- test/test.js
+++ test/test.js
@@ -29,26 +29,26 @@ pages = [
 ]
 
 pages.forEach(function (page) {
-    console.log(page.url)
-    
-    json = child_process.execSync('phantomjs pagetext.js ' + page.url).toString()
-    
-    result = JSON.parse(json)
-    assert(result, 'No result returned')
-    assert(result.hasOwnProperty('title'), 'No title property')
-    assert(result.title.length > 10, 'Title property too short')
-    assert(result.hasOwnProperty('text'), 'No text property')
-    assert(result.text.length > 1000, 'Text property too short')
-    assert(result.text.length < 20000, 'Text property too long')
-    assert(result.hasOwnProperty('html'), 'No html property')
-    assert(result.html.length > 1000, 'html property too short')
-    assert(result.html.length < 25000, 'html property too long')
-    console.log(result.text)
-    assert(result.text.indexOf('Continue reading the main story') == -1, 'Ads detected in text ("Continue reading the main story" found)')
-    
-    if (page.must_contain) {
-        page.must_contain.forEach(function (str) {
-            assert(result.text.indexOf(str) >= 0, 'Text "' + str + '" not fount')
-        })
-    }
+    child_process.exec('phantomjs pagetext.js ' + page.url, function (error, stdout) {
+        console.log(page.url)
+        result = JSON.parse(stdout.toString())
+
+        assert(result, 'No result returned')
+        assert(result.hasOwnProperty('title'), 'No title property')
+        assert(result.title.length > 10, 'Title property too short')
+        assert(result.hasOwnProperty('text'), 'No text property')
+        assert(result.text.length > 1000, 'Text property too short')
+        assert(result.text.length < 20000, 'Text property too long')
+        assert(result.hasOwnProperty('html'), 'No html property')
+        assert(result.html.length > 1000, 'html property too short')
+        assert(result.html.length < 25000, 'html property too long')
+        console.log(result.text)
+        assert(result.text.indexOf('Continue reading the main story') == -1, 'Ads detected in text ("Continue reading the main story" found)')
+        
+        if (page.must_contain) {
+            page.must_contain.forEach(function (str) {
+                assert(result.text.indexOf(str) >= 0, 'Text "' + str + '" not fount')
+            })
+        }
+    })
 })
