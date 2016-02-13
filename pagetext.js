6020846d979ac3c292874328a0d8dfb826057934
Jon Craton <jncraton@gmail.com>
2016-02-12 20:24:36 -0500

    Ignore form elements when searching for content

--- pagetext.js
+++ pagetext.js
@@ -51,7 +51,7 @@ page.open(system.args[1], function (status) {
                 }
 
                 // Remove extra elements
-                var els = document.querySelectorAll('meta,style,script,noscript,iframe,link,object')
+                var els = document.querySelectorAll('form,meta,style,script,noscript,iframe,link,object')
                 for (var i = 0; i < els.length; i++) {
                     els[i].parentNode.removeChild(els[i])
                 }
